2dcd80cae4884cd132e86df9320074fadc3018f1
Sarah Maston <maston@pantomath.com>
2015-12-16 16:28:24 -0500

    w3 html validation fixes

--- resources/views/partials/kpi-bar.blade.php
+++ resources/views/partials/kpi-bar.blade.php
@@ -1,4 +1,4 @@
-<section class="row kpi-bar">
+<div class="row kpi-bar">
 	@if(Auth::check())
 	<div class="col-md-12">
 		<span class="kpi-total-save">Total You've Saved : ${{$user_total_save}}</span>
@@ -7,4 +7,4 @@
 	@else
 	<div class="col-md-12 kpi-bar-blank"></div>
 	@endif
-</section>
+</div>
